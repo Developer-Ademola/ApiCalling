4a40fa7a90ff67e893b6181e7127a5f560335ab7
Dev-Ademola <112282908+Developer-Ademola@users.noreply.github.com>
2024-05-21 14:03:05 +0100

    ddddddd

--- EMSTutorial/Program.cs
+++ EMSTutorial/Program.cs
@@ -22,11 +22,21 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
-//}
+}
+else
+{
+    // Optionally enable Swagger in production with restricted access
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty;
+    });
+}
 
 app.UseHttpsRedirection();
 
