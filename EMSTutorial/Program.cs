536bd54eb62916cf7882c497a2debe85a232ec8b
Dev_Ademola <112282908+Developer-Ademola@users.noreply.github.com>
2024-05-23 00:59:52 +0100

    Update Program.cs

--- EMSTutorial/Program.cs
+++ EMSTutorial/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("SQLiteConnection")));
-//  Add injection Iservice and services
+//  Add injection Iservice and service
 builder.Services.AddScoped<ICampaignService, CampaignService>();
 
 builder.Services.AddControllers();
